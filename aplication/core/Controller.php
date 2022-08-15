7abb6cdcc9a6796f54bf4f195554ed84ba840f96
ryosuke-shibata-hub <ibis.stk@gmail.com>
2022-08-16 00:24:09 +0900

    20220816

---
+++ aplication/core/Controller.php
@@ -0,0 +1,76 @@
+<?php
+
+abstract class Controller
+{
+    protected $controller_name;
+    protected $action_name;
+    protected $application;
+    protected $request;
+    protected $response;
+    protected $session;
+    protected $db_manager;
+
+    public function __construct($application)
+    {
+        $this->controller_name = strtolower(substr(get_class($this), 0, -10));
+
+        $this->application = $application;
+        $this->request = $request->getRequest();
+        $this->response = $response->getResponse();
+        $this->session = $session->getSession();
+        $this->db_manager = $db_manager->getDbManager();
+    }
+
+    public function run($action, $params = array())
+    {
+        $this->action_name = $action;
+
+        $action_method = $action . 'Action';
+        if (!method_exists($this, $action_method)) {
+            $this->forward404();
+        }
+
+        $content = $this->$action_method($params);
+
+        return $content;
+    }
+
+    protected function render($variables = array(), $template = null, $layouts = 'layout')
+    {
+        $defaults = array(
+            'request' => $this->request,
+            'base_url' => $this->request->getBaseUrl(),
+            'session' => $this->session,
+        );
+
+        $view = new View($this->application->getViewDir(), $defaults);
+
+        if (is_null($template)) {
+            $template = $this->action_name;
+        }
+
+        $path = $this->controller_name . '/' .$template;
+
+        return $view->render($path, $variables, $layouts);
+    }
+
+    protected function forward404()
+    {
+        throw new HttpNotFoundException('Foreard 404 page from' . $this->controller_name .
+        '/' . $this->action_name);
+    }
+
+    protected function redirect($url)
+    {
+        if (!preg_match('#https?://#', $url)) {
+            $protocol = $this->request->isSl() ? 'https://' : 'http://';
+            $host = $this->request->getHost();
+            $base_url = $this->request->getBaseUrl();
+
+            $url = $protocol . $host . $base_url . $url;
+        }
+
+        $this->response->setStatusCode(302, 'Found');
+        $this->response->setHttpHeader('Location', $url);
+    }
+}
